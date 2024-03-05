be6d277ab1822819238123a61e2aef866649c778
BransonRT <10914976@uvu.edu>
2024-03-05 13:45:47 -0700

    Camera now follows the vehicle. Kind of...

--- DrivingGame/Assets/Scripts/FollowPlayer.cs
+++ DrivingGame/Assets/Scripts/FollowPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      transform.position = player.transform.position;  
     }
 }
