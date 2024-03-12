34f9468262a87ae49599be369b64bca089d2f9a4
BransonRT <10914976@uvu.edu>
2024-03-11 21:33:50 -0600

    Rearranged some of the code and added the y position

--- AnimalStamps/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ AnimalStamps/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -20;
+    private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 1;
+    private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -23,10 +23,11 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+       int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
