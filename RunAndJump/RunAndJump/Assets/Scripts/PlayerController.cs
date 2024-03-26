9a9fd2979c2e051ad66b53217b3430eaa80f2222
BransonRT <10914976@uvu.edu>
2024-03-26 14:05:07 -0600

    Added Music and SFX to the game and the script

--- RunAndJump/RunAndJump/Assets/Scripts/PlayerController.cs
+++ RunAndJump/RunAndJump/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -17,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent <Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
         
        
@@ -24,13 +28,14 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {//Player movement and jump/ also if player dies they won't be able to jump
+    {//Player movement and jump/ also if player dies they won't be able to jump. Dirt particles have been added
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
              playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
              isOnGround = false;
              playerAnim.SetTrigger("Jump_trig");
              dirtParticle.Stop();
+             playerAudio.PlayOneShot(jumpSound, 1.0f);
 
         }
     } 
@@ -49,6 +54,8 @@ public class PlayerController : MonoBehaviour
                 playerAnim.SetInteger("DeathType_int", 1);
                 explosionParticle.Play();
                 dirtParticle.Stop();
+                playerAudio.PlayOneShot(crashSound, 1.0f);
+            
             }
         
     }
