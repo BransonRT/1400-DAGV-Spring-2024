6c43a32450a5dd8f2a74b9b98ab0a7f64c32d259
BransonRT <10914976@uvu.edu>
2024-03-31 22:44:59 -0600

    Added player movement and boundaries

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    private float speed = 10.0f;
+    private float zBound= 8;
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        ConstrainPlayerPosition();
+
+        void MovePlayer()
+        {
+        // Allows player to move with default keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        //Player speed
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        }
+
+        void ConstrainPlayerPosition()
+        {
+        //Invokes a boundary that the player can't cross
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        }
     }
 }
