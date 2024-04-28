15f37f65ce521852576e0d72e55b9a65b67df85d
BransonRT <10914976@uvu.edu>
2024-04-28 10:53:17 -0600

    Added new scripts, made prefabs, added a game manager

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -40,6 +40,12 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
         }
+       private void OnCollisionEnter(Collision collision)
+       if(OnCollisionEnter.gameObject.CompareTag("Enemy"))
+       {
+        Debug.log("Got hit");
+       }
+
         }
     }
 }
